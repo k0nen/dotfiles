1a6b93e3bc9a9211a2086808ca292c00a2f3f73e
k0nen <k0nen@vllab6>
2022-01-03 13:15:03 +0900

    Install oh-my-zsh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 DOT_DIR=$PWD
 ZSH=$HOME/.oh-my-zsh
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+
 ln -sf $DOT_DIR/tmux.conf $HOME/.tmux.conf
 ln -sf $DOT_DIR/zshrc $HOME/.zshrc
 
